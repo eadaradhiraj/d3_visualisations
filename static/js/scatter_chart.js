85a2b9cf223d1e09db2c43eddd558a289705166d
eadaradhiraj <dhiraj.eadara@gramener.com>
2018-05-23 22:43:08 +0530

    ENH: Extra scatter plot

--- static/js/scatter_chart.js
+++ static/js/scatter_chart.js
@@ -1050,6 +1050,309 @@ var data1 = [{
     }
 ]
 
+var data2 = [{
+        "country": "United States",
+        "population": 323050000,
+        "expectancy": 78.94,
+        "cost": 9024.21,
+        "code": "US"
+    },
+    {
+        "country": "Switzerland",
+        "population": 8306200,
+        "expectancy": 82.85,
+        "cost": 6786.57,
+        "code": "CH"
+    },
+    {
+        "country": "Norway",
+        "population": 5213985,
+        "expectancy": 81.75,
+        "cost": 6081,
+        "code": "NO"
+    },
+    {
+        "country": "Netherlands",
+        "population": 16998003,
+        "expectancy": 81.3,
+        "cost": 5276.6,
+        "code": "NL"
+    },
+    {
+        "country": "Germany",
+        "population": 81459000,
+        "expectancy": 80.84,
+        "cost": 5119.21,
+        "code": "DE"
+    },
+    {
+        "country": "Sweden",
+        "population": 9858794,
+        "expectancy": 81.96,
+        "cost": 5065.16,
+        "code": "SE"
+    },
+    {
+        "country": "Ireland",
+        "population": 4635400,
+        "expectancy": 81.15,
+        "cost": 5001.32,
+        "code": "IE"
+    },
+    {
+        "country": "Austria",
+        "population": 8699730,
+        "expectancy": 81.34,
+        "cost": 4896,
+        "code": "AT"
+    },
+    {
+        "country": "Denmark",
+        "population": 5707251,
+        "expectancy": 80.55,
+        "cost": 4857.03,
+        "code": "DK"
+    },
+    {
+        "country": "Belgium",
+        "population": 11306030,
+        "expectancy": 80.59,
+        "cost": 4522.04,
+        "code": "BE"
+    },
+    {
+        "country": "Canada",
+        "population": 35985751,
+        "expectancy": 81.96,
+        "cost": 4495.69,
+        "code": "CA"
+    },
+    {
+        "country": "Luxembourg",
+        "population": 562,
+        "expectancy": 82.21,
+        "cost": 4478.97,
+        "code": "LU"
+    },
+    {
+        "country": "France",
+        "population": 64513242,
+        "expectancy": 82.37,
+        "cost": 4366.99,
+        "code": "FR"
+    },
+    {
+        "country": "Australia",
+        "population": 24026900,
+        "expectancy": 82.25,
+        "cost": 4206.85,
+        "code": "AU"
+    },
+    {
+        "country": "Japan",
+        "population": 126810000,
+        "expectancy": 83.59,
+        "cost": 4152.37,
+        "code": "JP"
+    },
+    {
+        "country": "United Kingdom",
+        "population": 65097000,
+        "expectancy": 81.06,
+        "cost": 3971.39,
+        "code": "UK"
+    },
+    {
+        "country": "Iceland",
+        "population": 332,
+        "expectancy": 82.06,
+        "cost": 3896.93,
+        "code": "IS"
+    },
+    {
+        "country": "Finland",
+        "population": 5490750,
+        "expectancy": 81.13,
+        "cost": 3871.39,
+        "code": "FI"
+    },
+    {
+        "country": "New Zealand",
+        "population": 4671020,
+        "expectancy": 81.4,
+        "cost": 3537.26,
+        "code": "NZ"
+    },
+    {
+        "country": "Italy",
+        "population": 60676361,
+        "expectancy": 82.69,
+        "cost": 3206.83,
+        "code": "IT"
+    },
+    {
+        "country": "Spain",
+        "population": 46423064,
+        "expectancy": 83.08,
+        "cost": 3053.07,
+        "code": "ES"
+    },
+    {
+        "country": "Slovenia",
+        "population": 2069982,
+        "expectancy": 80.52,
+        "cost": 2598.91,
+        "code": "SI"
+    },
+    {
+        "country": "Portugal",
+        "population": 10374822,
+        "expectancy": 80.72,
+        "cost": 2583.84,
+        "code": "PT"
+    },
+    {
+        "country": "Israel",
+        "population": 8476600,
+        "expectancy": 82.15,
+        "cost": 2547.4,
+        "code": "IL"
+    },
+    {
+        "country": "Czech Republic",
+        "population": 10546120,
+        "expectancy": 78.28,
+        "cost": 2386.34,
+        "code": "CZ"
+    },
+    {
+        "country": "South Korea",
+        "population": 51555409,
+        "expectancy": 82.16,
+        "cost": 2361.44,
+        "code": "KR"
+    },
+    {
+        "country": "Greece",
+        "population": 10846979,
+        "expectancy": 81.29,
+        "cost": 2220.11,
+        "code": "GR"
+    },
+    {
+        "country": "Slovakia",
+        "population": 5424058,
+        "expectancy": 76.71,
+        "cost": 1970.52,
+        "code": "SK"
+    },
+    {
+        "country": "Hungary",
+        "population": 9849000,
+        "expectancy": 75.87,
+        "cost": 1796.6,
+        "code": "HU"
+    },
+    {
+        "country": "Estonia",
+        "population": 1311759,
+        "expectancy": 77.24,
+        "cost": 1724.51,
+        "code": "EE"
+    },
+    {
+        "country": "Lithuania",
+        "population": 2881705,
+        "expectancy": 73.97,
+        "cost": 1720.84,
+        "code": "LT"
+    },
+    {
+        "country": "Chile",
+        "population": 18191900,
+        "expectancy": 81.5,
+        "cost": 1688.52,
+        "code": "CL"
+    },
+    {
+        "country": "Poland",
+        "population": 38484000,
+        "expectancy": 77.25,
+        "cost": 1624.87,
+        "code": "PL"
+    },
+    {
+        "country": "Costa Rica",
+        "population": 4832234,
+        "expectancy": 79.4,
+        "cost": 1393.95,
+        "code": "CR"
+    },
+    {
+        "country": "Russia",
+        "population": 146544710,
+        "expectancy": 70.37,
+        "cost": 1368.75,
+        "code": "RU"
+    },
+    {
+        "country": "Latvia",
+        "population": 1968600,
+        "expectancy": 74.19,
+        "cost": 1295.01,
+        "code": "LV"
+    },
+    {
+        "country": "South Africa",
+        "population": 54956900,
+        "expectancy": 57.18,
+        "cost": 1146.47,
+        "code": "ZA"
+    },
+    {
+        "country": "Mexico",
+        "population": 122273500,
+        "expectancy": 76.72,
+        "cost": 1052.66,
+        "code": "MX"
+    },
+    {
+        "country": "Turkey",
+        "population": 78741053,
+        "expectancy": 75.16,
+        "cost": 990.19,
+        "code": "TR"
+    },
+    {
+        "country": "Colombia",
+        "population": 48570200,
+        "expectancy": 73.99,
+        "cost": 964.5,
+        "code": "CO"
+    },
+    {
+        "country": "China",
+        "population": 1375410000,
+        "expectancy": 75.78,
+        "cost": 730.52,
+        "code": "CN"
+    },
+    {
+        "country": "Indonesia",
+        "population": 258705000,
+        "expectancy": 68.89,
+        "cost": 302.12,
+        "code": "ID"
+    },
+    {
+        "country": "India",
+        "population": 1285930000,
+        "expectancy": 68.01,
+        "cost": 267.41,
+        "code": "IN"
+    }
+]
+
 function scatter_chart(config) {
     var data = config.data,
         x = config.x,
@@ -1066,8 +1369,6 @@ function scatter_chart(config) {
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-
-    // The API for scales have changed in v4. There is a separate module d3-scale which can be used instead. The main change here is instead of d3.scale.linear, we have d3.scaleLinear.
     var xScale = d3.scaleLinear()
         .range([0, width]);
 
@@ -1078,14 +1379,13 @@ function scatter_chart(config) {
     var radius = d3.scaleSqrt()
         .range([2, 5]);
 
-    // the axes are much cleaner and easier now. No need to rotate and orient the axis, just call axisBottom, axisLeft etc.
     var xAxis = d3.axisBottom()
         .scale(xScale);
 
     var yAxis = d3.axisLeft()
         .scale(yScale);
 
-    // again scaleOrdinal
+    // adding arbitrary colours
     var color = d3.scaleOrdinal(d3.schemeCategory20);
 
     xScale.domain(d3.extent(data, function (d) {
@@ -1103,23 +1403,23 @@ function scatter_chart(config) {
         return d[r];
     })).nice();
 
-    // adding axes is also simpler now, just translate x-axis to (0,height) and it's alread defined to be a bottom axis. 
+    // translate x-axis to (0,height) and it's defined to be a bottom axis. 
     svg.append('g')
         .attr('transform', 'translate(0,' + height + ')')
-        .attr('class', 'x axis')
+        .attr('class', 'x axis'+" "+config.selector)
         .call(xAxis);
 
     // y-axis is translated to (0,0)
     svg.append('g')
         .attr('transform', 'translate(0,0)')
-        .attr('class', 'y axis')
+        .attr('class', 'y axis'+" "+config.selector)
         .call(yAxis);
 
 
-    var bubble = svg.selectAll('.bubble')
+    var bubble = svg.selectAll('.bubble'+"."+config.selector)
         .data(data)
         .enter().append('circle')
-        .attr('class', 'bubble')
+        .attr('class', 'bubble'+" "+config.selector)
         .attr('cx', function (d) {
             return xScale(d[x]);
         })
@@ -1145,23 +1445,21 @@ function scatter_chart(config) {
     svg.append('text')
         .attr('x', 10)
         .attr('y', 10)
-        .attr('class', 'label')
+        .attr('class', 'label'+" "+config.selector)
         .text(y);
-
 
     svg.append('text')
         .attr('x', width)
         .attr('y', height - 10)
         .attr('text-anchor', 'end')
-        .attr('class', 'label')
+        .attr('class', 'label'+" "+config.selector)
         .text(x);
 
-    // I feel I understand legends much better now.
     // define a group element for each color i, and translate it to (0, i * 20). 
-    var legend = svg.selectAll('legend')
+    var legend = svg.selectAll('legend'+"."+config.selector)
         .data(color.domain())
         .enter().append('g')
-        .attr('class', 'legend')
+        .attr('class', 'legend'+" "+config.selector)
         .attr('transform', function (d, i) {
             return 'translate(0,' + i * 20 + ')';
         });
@@ -1188,7 +1486,7 @@ function scatter_chart(config) {
 
     // d3 has a filter fnction similar to filter function in JS. Here it is used to filter d3 components.
     legend.on('click', function (type) {
-        d3.selectAll('.bubble')
+        d3.selectAll('.bubble'+"."+config.selector)
             .style('opacity', 0.15)
             .filter(function (d) {
                 return d[config.key] == type;
@@ -1212,4 +1510,20 @@ var config1 = {
     r: "PetalLength"
 }
 
+var config2 = {
+    margin: {
+        top: 30,
+        right: 50,
+        bottom: 40,
+        left: 40
+    },
+    selector: "scatter_chart2",
+    data: data2,
+    key: "country",
+    x: "cost",
+    y: "expectancy",
+    r: "population"
+}
+
 scatter_chart(config1)
+scatter_chart(config2)
